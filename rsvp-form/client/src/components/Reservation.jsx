64a606673d5812c5d27798eb3d2e1c0cd509a3b1
Tina Xing <tinaxing14@gmail.com>
2020-05-31 22:24:39 -0400

    added customized form validation

--- rsvp-form/client/src/components/Reservation.jsx
+++ rsvp-form/client/src/components/Reservation.jsx
@@ -4,30 +4,71 @@ class Reservation extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      firstName: '',
-      lastName: '',
-      emailAddress:'',
-      numberOfGuests: ''
+      firstName: null,
+      lastName: null,
+      emailAddress: null,
+      numberOfGuests: null,
+      errors: {
+        firstName: "",
+        lastName: "",
+        emailAddress: "",
+        numberOfGuests: "",
+      },
     };
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleInputChange(e) {
-    e.preventDefault();
+    const validEmailRegex = RegExp(/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i);
     const { name, value } = e.target;
-    this.setState({
-      [name]: value
-    })
+    let errors = this.state.errors;
+    switch (name) {
+      case "firstName":
+        errors.firstName =
+          value.length < 2 ? "first name must be 2 characters long!" : "";
+        break;
+      case "lastName":
+        errors.lastName =
+          value.length < 2 ? "last name must be 2 characters long!" : "";
+        break;
+      case "emailAddress":
+        errors.emailAddress = validEmailRegex.test(value)
+          ? ""
+          : "email is not valid";
+        break;
+      case "numberOfGuests":
+        Number(value) < 0
+        ? "must have more than one guest"
+        : "";
+        break;
+      default:
+        break;
+    }
+    this.setState({errors, [name]: value,}, () => {
+      console.log(errors)
+    });
   }
 
-  handleSubmit() {
-    this.props.handleInsert(this.state)
+  handleSubmit(e) {
+    e.preventDefault();
+    const validateForm = (form0bj) => {
+      let valid = true;
+      Object.values(form0bj).forEach((item) => {
+        if(item.length > 0){
+          valid = false
+        }
+      })
+      return valid;
+    }
+    if(validateForm(this.state.errors)){
+      this.props.handleInsert(this.state);
+    }
   }
 
   render() {
     return (
-      <form onSubmit = {this.handleSubmit}>
+      <form onSubmit={this.handleSubmit}>
         <label>
           First Name
           <input
@@ -37,6 +78,8 @@ class Reservation extends React.Component {
             onChange={this.handleInputChange}
           />
         </label>
+        {this.state.errors.firstName.length > 0 && <span>{this.state.errors.firstName}</span>}
+        <br/>
         <label>
           Last Name
           <input
@@ -46,7 +89,9 @@ class Reservation extends React.Component {
             onChange={this.handleInputChange}
           />
         </label>
+        {this.state.errors.lastName.length > 0 && <span>{this.state.errors.lastName}</span>}
         <br />
+        {}
         <label>
           Email Address
           <input
@@ -56,6 +101,7 @@ class Reservation extends React.Component {
             onChange={this.handleInputChange}
           />
         </label>
+        {this.state.errors.emailAddress.length > 0 && <span>{this.state.errors.emailAddress}</span>}
         <br />
         <label>
           Number of guests:
